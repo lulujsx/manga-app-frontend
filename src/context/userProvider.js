2f299e15bce1ce26ac05a9f743e02f0764877093
Luana Vallejos <luanalorenavallejos@gmail.com>
2022-01-22 18:33:46 -0300

    modificacion

--- src/context/userProvider.js
+++ src/context/userProvider.js
@@ -1,11 +1,11 @@
 import React, { useContext, useState } from 'react'
 
 const Context = React.createContext({
-    user:localStorage.getItem("user lo q  qcyo no c")??"",setUser:() =>{}
+    user:localStorage.getItem("user lo q  qcyo no c")??"{}",setUser:() =>{}
 })
 
 const UserContext = (props) => {
-    const localUser = localStorage.getItem("user lo q  qcyo no c")??""
+    const localUser = localStorage.getItem("user lo q  qcyo no c")??"{}"
     const [user, setUser_] = useState(JSON.parse(localUser))
 
     const setUser = (data) => {
